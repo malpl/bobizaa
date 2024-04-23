2d35169a6e7942b0600781d7d1df4d88dbaad3d7
Mmmmssnb <166040824+Mmmmssnb@users.noreply.github.com>
2024-04-24 01:19:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('BOBIZA', {
+say('malik', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
