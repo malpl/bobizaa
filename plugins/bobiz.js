0432942989dcdcc68c88cd8c427833b1230f2b0b
Mmmmssnb <166040824+Mmmmssnb@users.noreply.github.com>
2024-04-30 16:48:10 +0300

    Update bobiz.js

--- plugins/bobiz.js
+++ plugins/bobiz.js
@@ -8,7 +8,7 @@ let handler = async (m, {
 }) => {
     await m.reply(wait)
     let res = await YesNo()
-    let stiker = await createSticker(false, res.image, "bobiza ", (res.answer).toUpperCase(), 30)
+    let stiker = await createSticker(false, res.image, "malik <01030825867> ", (res.answer).toUpperCase(), 30)
         
     try {
         await m.reply(stiker)
