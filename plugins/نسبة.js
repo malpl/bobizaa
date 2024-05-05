7028c8f0242b259f4e428b38dff02916ba498172
Mmmmssnb <166040824+Mmmmssnb@users.noreply.github.com>
2024-05-05 17:28:29 +0300

    Update نسبة.js

--- plugins/نسبة.js
+++ plugins/نسبة.js
@@ -4,7 +4,7 @@ let love = `
 *اطلب منه/ا ان تصبح صديقتك*
 `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
-handler.help = ['love']
+handler.help = ['نسبة']
 handler.tags = ['fun']
 handler.command = /^(نسبه|نسبة)$/i
 export default handler
