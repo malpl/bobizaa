9819201c7f0d7a9038bbe619ddc29f3d23758654
Mmmmssnb <166040824+Mmmmssnb@users.noreply.github.com>
2024-05-06 03:06:34 +0300

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -5,15 +5,15 @@ let handler = async (m, { conn, text, isOwner }) => {
     if (!code) throw 'الرابط غير صالح'
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`تم الانضمام للمجموعة بنجاح ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\nتابع صاحب البوت في حسابه\`)
+    m.reply(`تم الانضمام للمجموعة بنجاح ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\n\`)
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
 }
-handler.help = ['join']
+handler.help = ['انضم']
 handler.tags = ['owner']
 
-handler.command = /^join$/i
+handler.command = /^انضم|ادخل$/i
 handler.rowner = true
 
 export default handler
