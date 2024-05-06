ff85fbdcfeb14a32250d860f0c5f0c69ffaa131f
Mmmmssnb <166040824+Mmmmssnb@users.noreply.github.com>
2024-05-06 15:31:27 +0300

    Update الوداع.js

--- plugins/الوداع.js
+++ plugins/الوداع.js
@@ -1,12 +1,11 @@
-let handler = async (m, { conn, text, isROwner, isOwner }) => {
-if (text) {
-global.db.data.chats[m.chat].sBye = text
-m.reply('تم')
-} else throw `ادخل رسالة الي تبغى تحطها
-
-}
-handler.help = ['لوداع<text>']
-handler.tags = ['group']
-handler.command = ['لوداع'] 
-handler.admin = true
-export default handler
+const handler = async (m, {conn, text, isROwner, isOwner}) => {
+  if (text) {
+    global.db.data.chats[m.chat].sBye = text;
+    m.reply('*[❗] تم تكوين رسالة وداع بشكل صحيح لهذه المجموعة*');
+  } else throw `*[❗] قم بوضع رساله، استخدم:*\n*- @user (لمنشن العضو)*`;
+};
+handler.help = ['تغيرالوداع'];
+handler.tags = ['group'];
+handler.command = ['تغير-الوداع', 'تغيرالوداع'];
+handler.admin = true;
+export default handler;
