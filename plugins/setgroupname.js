317113349827d676c072bb9e938772dce62b6f53
Mmmmssnb <166040824+Mmmmssnb@users.noreply.github.com>
2024-05-11 23:50:10 +0300

    Update setgroupname.js

--- plugins/setgroupname.js
+++ plugins/setgroupname.js
@@ -4,9 +4,9 @@ if (!text) return conn.reply(m.chat, 'هذا الأمر خاص بتغيير إس
 await conn.groupUpdateSubject(m.chat, text)
 conn.reply(m.chat, `🚩 ${text ? `${text} *هو الاسم الجديد لهذه المجموعة*\n` : '*لم يعطوا اسما*'}`, m,  )
 }
-handler.help = ['setgroupname']
+handler.help = ['تغير اسم البار']
 handler.tags = ['owner']
-handler.command = /^setgroupname$/i
+handler.command = /^تغير اسم البار$/i
 
 handler.botAdmin = true
 handler.group = true
