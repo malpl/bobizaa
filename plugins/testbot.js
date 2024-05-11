b53ef40c6376613f8995bbdc553f3fa64a00f1fa
Mmmmssnb <166040824+Mmmmssnb@users.noreply.github.com>
2024-05-12 02:28:58 +0300

    Update testbot.js

--- plugins/testbot.js
+++ plugins/testbot.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `بوبيزة متصلة الان بالانترنيت \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \n`
+let info = `🧛 مالك خطر اولاين   \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \n`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 65,
       isForwarded: true, externalAdReply: { title: author, body: bottime,thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
       await conn.sendMessage(m.chat, {
