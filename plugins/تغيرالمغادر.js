f0209aefc4869ba64f66a432afadae9969203ec7
malpl <168739286+malpl@users.noreply.github.com>
2024-05-18 03:51:19 -0700

    Update تغيرالمغادر.js

--- plugins/تغيرالمغادر.js
+++ plugins/تغيرالمغادر.js
@@ -1,15 +1,11 @@
-//import db from '../lib/database.js'
-
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
-  if (text) {
-    global.db.data.chats[m.chat].sBye = text
-    m.reply('*تـــم تـحديــث رســالــة الـوداع !*')
-  } else throw `*أدخــل الــرسالــه !*\n*أكــتب @user اذا اردت ان يــكون بــها مـنشـن !*`
+if (text) {
+global.db.data.chats[m.chat].sBye = text
+m.reply('*[❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙳𝙴𝚂𝙿𝙴𝙳𝙸𝙳𝙰 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴 𝙿𝙰𝚁𝙰 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾*')
+} else throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙳𝙴𝚂𝙿𝙴𝙳𝙸𝙳𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙰𝙶𝚁𝙴𝙶𝙰𝚁, 𝚄𝚂𝙴:*\n*- @user (mención)*`
 }
 handler.help = ['المغادره']
 handler.tags = ['group']
 handler.command = ['المغادره'] 
 handler.admin = true
-handler.owner = false
-
 export default handler
